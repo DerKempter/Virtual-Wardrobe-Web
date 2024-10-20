1473fd7011ee1d0567ad524500aaf03b79c7defc
DerKempter <jkempter@students.uni-mainz.de>
2024-10-20 13:57:54 +0200

    backend test update

--- wardrobe-backend/main.py
+++ wardrobe-backend/main.py
@@ -1,13 +1,28 @@
 from flask import Flask, request, jsonify
 from PIL import Image
-import sqlite3
 import io
+
+import db
 
 app = Flask("Virtual Wardrobe Web")
 
-@app.route('/', methods=['GET'])
+@app.route('/test-route', methods=['GET'])
 def hello_world():
-    return jsonify({'result': 'hello world!'})
+    err_coll = []
+    try:
+        _id = int(request.args.get('id'))
+    except ValueError as ve:
+        err_coll.append(1)
+    value = request.args.get('value')
+    if not value:
+        err_coll.append(3)
+
+    if len(err_coll) > 1:
+        return jsonify({'success': False, 'errs': err_coll})
+    # DB ACCESS Gedöns
+    result = db.access_db()
+
+    return jsonify({'success': True, 'id': _id, 'value': value, 'result': result})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
