95ee1b1cfb829e81b2109a624a2752a03b705ea1
DerKempter <jkempter@students.uni-mainz.de>
2024-10-20 14:43:51 +0200

    add sidebar

--- wardrobe-frontend/src/app/app.component.ts
+++ wardrobe-frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {MatDrawer, MatDrawerContainer} from "@angular/material/sidenav";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDrawerContainer, MatDrawer, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wardrobe-frontend';
+  title = 'Virtual Wardrobe';
+  router: Router = inject(Router)
+
+  isSidebarOpen = false;
+
+  toggleSidebar = () => {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  goToTarget = (target: string) => {
+    this.router.navigate([target]).then()
+  }
 }
